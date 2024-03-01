73d88140e346ac0fe949b95aaa28ded5e42e2c19
LikeManTV <56754106+LikeManTV@users.noreply.github.com>
2024-03-01 17:52:22 +0100

    Added Portugese translation
    Credit: dnrcs

---
+++ locales/pt.lua
@@ -0,0 +1,82 @@
+Language['pt'] = {
+    keybind_desc = 'Abrir menu de Equipe',
+    toggle_tags = 'Tag alterada para %s!',
+
+    -- MAIN MENU
+    main_menu_title = 'MENU DE CLÃ',
+    main_menu_desc = 'Tag: [%s]\n Membros: %s',
+    main_menu_invite_title = 'CONVIDAR PLAYER',
+    main_menu_invite_desc = 'Clique para convidar players.',
+    main_menu_manage_title = 'ADMINISTRAR PLAYER',
+    main_menu_manage_desc = 'Clique para administrar players.',
+    main_menu_delete_title = 'DELETAR CLÃ',
+    main_menu_delete_desc = 'Clique para deletar o clã.',
+    main_menu_settings_title = 'CONFIGURAÇÕES',
+    main_menu_settings_desc = 'Clique para abrir as configurações.',
+    main_menu_leave_title = 'SAIR',
+    main_menu_leave_desc = 'Clique para sair do clã.',
+    main_menu_invites_title = 'ABRIR CONVITES',
+    main_menu_invites_desc = 'Clique para abrir os convites.',
+
+    -- CREATING A CREW
+    main_menu_create_title = 'CRIAR CLÃ',
+    main_menu_create_desc = 'Clique para criar um clã.',
+    create_name_desc = 'Digite o nome..',
+    create_tag_desc = 'Digite a TAG.. (4 chars.)',
+    create_success = 'Clã %s criado com sucesso!',
+
+    -- CREW DELETION
+    delete_confirmation_title = 'DELETAR CLÃ',
+    delete_confirmation_desc = 'Tem certeza que deseja deletar o seu clã?',
+    delete_success = 'Clã %s deletado com sucesso!',
+
+    -- INVITES
+    invites_title = 'CLÃ > CONVITES',
+    invites_btn_desc = 'Clique para aceitar.',
+    invites_received_new = 'Você recebeu um convite de clã, aperte F4 para abrir o menu!',
+    invites_success = 'Você entrou para o clã: %s',
+
+    -- MEMBER LIST
+    member_list_title = 'CLÃ > MEMBROS',
+    member_list_owner_title = '%s (Você)',
+
+    -- MANAGING MEMBERS
+    manage_title = 'CLÃ > ADMINISTRAÇÃO',
+    manage_btn_desc = 'Clique para administrar os players.',
+    manage_no_players = 'Não há ninguém para gerenciar.',
+
+    -- MEMBER SETTINGS
+    member_title = 'MEMBROS > %s',
+    member_rank_title = 'ALTERAR CARGO',
+    member_rank_desc = 'Cargo: %s',
+    member_kick_title = 'EXPULSAR',
+    member_kick_desc = 'Clique para expulsar do clã.',
+    member_kick_confirmation_title = 'EXPULSAR PLAYER',
+    member_kick_confirmation_desc = 'Você tem certeza que quer expulsar o membro %s?',
+    member_kick_success = 'Player explulso com sucesso!',
+    player_kicked = 'Você foi expulso do seu clã!',
+
+    -- INVITING PLAYERS
+    invite_title = 'CLÃ > CONVIDAR',
+    invite_btn_desc = 'Clique para convidar.',
+    invite_success = 'Você convidor o player %s para o clã!',
+
+    -- SETTINGS
+    settings_title = 'CLÃ > CONFIGURAÇÕES',
+    settings_btn_rename_title = 'RENOMEAR',
+    settings_btn_rename_desc = 'Clique para renomear.',
+    settings_btn_tag_title = 'ALTERAR TAG',
+    settings_btn_rename_desc = 'Clique para alterar a TAG.',
+    rename_success = 'O seu clã foi renomeado para: %s Crew',
+    tag_success = 'A TAG do seu clã foi renomada para: %s',
+
+    -- ERRORS
+    error_name_used = 'Este nome ja está em uso..',
+    error_tag_used = 'Esta TAG já está em uso..',
+    error_no_invites = 'Você não tem convites!',
+    error_nobody_around = 'Não há jogadores por perto!',
+    error_already_invited = 'Você já convidou este jogador.',
+    error_player_in_crew = 'Este jogador já faz parte de outro clã.',
+    error_limit_reached = 'Você atingiu o limite de jogadores no clã! (%s)',
+    error_no_crew = 'Você não possui um clã!',
+}
