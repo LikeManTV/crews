63ddeb0ca17f76190b9e6b67c7dab3ae4e714243
LikeManTV <56754106+LikeManTV@users.noreply.github.com>
2024-03-01 19:41:52 +0100

    fix: Added missing functions for ESX

--- bridge/esx/server.lua
+++ bridge/esx/server.lua
@@ -9,3 +9,11 @@ end
 function getPlayerFromIdentifier(identifier)
     return ESX.GetPlayerFromIdentifier(identifier)
 end
+
+function getAllPlayers()
+    return ESX.GetExtendedPlayers()
+end
+
+function getPlayerData(player)
+    return {ped = GetPlayerPed(player.source), identifier = player.identifier, coords = player.getCoords(), name = player.firstName .. ' ' .. player.lastName}
+end
