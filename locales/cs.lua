d9b1815896e60aa0a3e1cf41fc7ef7c04119b515
LikeManTV <56754106+LikeManTV@users.noreply.github.com>
2024-03-01 18:06:04 +0100

    Added Czech translation

---
+++ locales/cs.lua
@@ -0,0 +1,84 @@
+Language['cs'] = {
+    keybind_desc = 'Otevrit crew menu',
+    toggle_tags = 'Crew tagy jsou nyní %s!',
+    tags_enabled = 'zapnuty',
+    tags_disabled = 'vypnuty',
+
+    -- MAIN MENU
+    main_menu_title = 'CREW MENU',
+    main_menu_desc = 'Tag: [%s]\n Členové: %s',
+    main_menu_invite_title = 'POZVAT HRÁČE',
+    main_menu_invite_desc = 'Klikni pro pozvání hráčů.',
+    main_menu_manage_title = 'SPRAVOVAT ČLENY',
+    main_menu_manage_desc = 'Klikni pro správu členů.',
+    main_menu_delete_title = 'SMAZAT CREW',
+    main_menu_delete_desc = 'Klikni pro smazání crew.',
+    main_menu_settings_title = 'NASTAVENÍ',
+    main_menu_settings_desc = 'Klikni pro otevření nastavení.',
+    main_menu_leave_title = 'OPUSTIT',
+    main_menu_leave_desc = 'Klikni pro opuštění crew.',
+    main_menu_invites_title = 'ZOBRATI POZVÁNKY',
+    main_menu_invites_desc = 'Klikni pro zobrazení pozvánek.',
+
+    -- CREATING A CREW
+    main_menu_create_title = 'VYTVOŘIT CREW',
+    main_menu_create_desc = 'Klikni pro vytvoření crew.',
+    create_name_desc = 'Zadejte jméno..',
+    create_tag_desc = 'Zadejte tag.. (4 znaky)',
+    create_success = 'Úspěšně vytvořeno: %s',
+
+    -- CREW DELETION
+    delete_confirmation_title = 'SMAZAT CREW',
+    delete_confirmation_desc = 'Opravdu chcete smazat tuto crew?',
+    delete_success = 'Úspěšně smazáno: %s',
+
+    -- INVITES
+    invites_title = 'CREW > POZVÁNKY',
+    invites_btn_desc = 'Klikni pro přijetí.',
+    invites_received_new = 'Obdrželi jste pozvánku do crew!',
+    invites_success = 'Připojili jste se k: %s',
+
+    -- MEMBER LIST
+    member_list_title = 'CREW > ČLENOVĚ',
+    member_list_owner_title = '%s (Ty)',
+
+    -- MANAGING MEMBERS
+    manage_title = 'CREW > SPRÁVA',
+    manage_btn_desc = 'Klikni pro správu člena.',
+    manage_no_players = 'Není tu nikdo ke správě.',
+
+    -- MEMBER SETTINGS
+    member_title = 'ČLENOVÉ > %s',
+    member_rank_title = 'ZMĚNIT POZICI',
+    member_rank_desc = 'Pozice: %s',
+    member_kick_title = 'VYHODIT',
+    member_kick_desc = 'Klikni pro vyhození.',
+    member_kick_confirmation_title = 'VYHODIT HRÁČE',
+    member_kick_confirmation_desc = 'Opravdu chcete vyhodit %s?',
+    member_kick_success = 'Hráč byl úspěšně vyhozen!',
+    player_kicked = 'Byli jste vyhozeni z crew!',
+
+    -- INVITING PLAYERS
+    invite_title = 'CREW > POZVAT HRÁČE',
+    invite_btn_desc = 'Klikni pro pozvání.',
+    invite_success = 'Pozvali jste: %s do crew!',
+
+    -- SETTINGS
+    settings_title = 'CREW > NASTAVENÍ',
+    settings_btn_rename_title = 'PŘEJMENOVAT',
+    settings_btn_rename_desc = 'Klikni pro změnu názvu.',
+    settings_btn_tag_title = 'ZMĚNIT TAG',
+    settings_btn_rename_desc = 'Klikni pro změnu tagu.',
+    rename_success = 'Vaše crew byla přejmenována na: %s Crew',
+    tag_success = 'Váč crew tag byl změněn na: %s',
+
+    -- ERRORS
+    error_name_used = 'Tento název je již zabraný..',
+    error_tag_used = 'Tento tag je již zabraný..',
+    error_no_invites = 'Nemáte žádné pozvánky!',
+    error_nobody_around = 'V okolí není žádný hráč!',
+    error_already_invited = 'Tohoto hráče jste již pozvali.',
+    error_player_in_crew = 'Tento hráč je již členem jiné crew.',
+    error_limit_reached = 'Dosáhli jste početního limitu členů! (%s)',
+    error_no_crew = 'Nejste v žádné crew!',
+}
